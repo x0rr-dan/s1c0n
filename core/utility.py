e08ac1bd93dbe39dbafabaa3ef4037583532af6e
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2025-01-19 13:50:16 +0700

    Update utility.py

--- core/utility.py
+++ core/utility.py
@@ -1,4 +1,5 @@
 from os import system, path, mkdir
+import sys, requests, re
 from core.color import Color
 # 1.3 wellcome screen
 def logo():
@@ -7,16 +8,34 @@ def logo():
     \t              ┏━┓╺┓ ┏━╸┏━┓┏┓╻
     \t              ┗━┓ ┃ ┃  ┃┃┃┃┗┫
     \t              ┗━┛╺┻╸┗━╸┗━┛╹ ╹ v1.8
-
-                        Simple Recon
-                        Coded by {Color.reset}{Color.red}{Color.bold}x0r{Color.yellow}{Color.bold}\n\t       https://github.com/x0rr-dan/s1c0n{Color.red}{Color.bold}\n\t           Dinus Open Source Community{Color.red}""")
+{Color.yellow}{Color.bold}\n\t       https://github.com/x0rr-dan/s1c0n{Color.red}{Color.bold}\n\t           Dinus Open Source Community{Color.red}{Color.reset}""")
 # 1.4 help
 def break_and_help():
-    print("\n\t   [?] Usage example: sicon -u target.com")
+    print(f"""{Color.bold}{Color.green}S1c0n{Color.reset} is a {Color.bold}{Color.green}recon tool{Color.reset} designed to assist cybersecurity professionals and penetration testers in identifying potential vulnerabilities in web servers. By integrating various scanning and recon tools, s1c0n simplifies the process of information gathering and security assessment on web infrastructure.\n""")
 
 # 1.5 clean file that doesnt use
 def clean(extension):
     system(f"rm -rf .list*.{extension}")
+
+def make_sure(proxy=None):
+    proxies = {"http": proxy, "https": proxy} if proxy else None
+    tor_url = "https://check.torproject.org/"
+    try:
+        response = requests.get(tor_url, proxies=proxies)
+        if response.status_code == 200:
+            res = response.text
+            match = re.search(r'Congratulations\. This browser is configured to use Tor', res)
+            if match:
+                return True
+            else:
+                return False
+        else:
+            return False
+    except requests.exceptions.RequestException:
+        return False
+        # print("[!] An error occurred while checking Tor:", e)
+        # exit()
+
 
 # 1.6 create folder report
 def create_report_folder(target):
