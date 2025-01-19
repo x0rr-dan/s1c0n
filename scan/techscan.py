2f46add0cbc4b286f5e8624380edb87167dbfc60
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2025-01-19 13:46:40 +0700

    Update techscan.py
    add custom proxy, custom user-agent, make sure target using http or https

--- scan/techscan.py
+++ scan/techscan.py
@@ -1,34 +1,10 @@
 from core.color import Color
-import requests
-import builtwith
-import random
+from core.random_ag import rangent
+import requests, builtwith, subprocess
 from requests.exceptions import Timeout, RequestException
 
-
-# Fungsi untuk membaca User-Agent dari file
-def load_user_agents(file_path):
-    try:
-        with open(file_path, 'r', encoding='utf-8') as file:
-            # Membaca setiap baris dan menghapus spasi kosong
-            user_agents = [line.strip() for line in file.readlines() if line.strip()]
-        return user_agents
-    except FileNotFoundError:
-        print(f"{Color.red}File {file_path} not found.{Color.reset}")
-        return []
-
-
-def more_info(target):
-    # Menentukan path file user_agents.txt
-    user_agents_file_path = "core/user_agents.txt"
-
-
-    # Memuat User-Agent dari file
-    user_agents = load_user_agents(user_agents_file_path)
-
-    if not user_agents:
-        print(f"{Color.red}No User-Agent data available. Exiting...{Color.reset}")
-        return
-
+def more_info(target, user_agent=None, proxy=None):
+    proxies = {"http": proxy, "https": proxy} if proxy else None
     try:
         with open(f"report_{target}/subdomain.txt", 'r', encoding='utf-8') as r:
             read_subdo = r.readlines()
@@ -38,16 +14,20 @@ def more_info(target):
         output_file_path = f"report_{target}/subdomain_with_tech.txt"
         with open(output_file_path, 'w', encoding='utf-8') as output_file:
             for url in read_subdo:
-                url = f"https://{url.strip()}"
+                url_mentah = url.strip()
+                url = subprocess.check_output((f"echo {url.strip()} | httprobe -prefer-https"), shell=True, text=True).strip()
+                # print(url)
+                if not url:
+                    url = f"http://{url_mentah}" # sometimes httprobe doesnt return anything
                 technologies_str = ""
 
                 try:
-                    # Pilih User-Agent secara acak dari daftar yang telah dimuat
-                    user_agent = random.choice(user_agents)
-                    headers = {"User-Agent": user_agent}
-
+                    if user_agent:
+                        headers = {"User-Agent": user_agent}
+                    else:
+                        headers = {"User-Agent": rangent()}
                     # Permintaan HTTP dengan User-Agent kustom
-                    r = requests.get(url, headers=headers, timeout=10)
+                    r = requests.get(url, headers=headers, timeout=20, proxies=proxies)
                     if r.status_code == 200:
                         data = builtwith.builtwith(url)
                         keys_of_interest = ['programming-language', 'cms', 'web-servers', 'javascript-frameworks',
@@ -63,14 +43,14 @@ def more_info(target):
 
                 except Timeout:
                     technologies_str = f"{Color.red}Timeout{Color.reset}"
+                except ConnectionError as e:
+                    technologies_str = f"{Color.red}Network error: {str(e)}{Color.reset}"
                 except RequestException as e:
-                    technologies_str = f"{Color.red}Failed to retrieve data ({str(e)}){Color.reset}"
+                    technologies_str = f"{Color.red}Host Offline{Color.reset}"
 
-                # Cetak hasil ke terminal
-                print(
-                    f"{Color.green}\t    -> {Color.reset}{Color.bold}{url} {Color.reset}| {Color.bold}{Color.green}[ {technologies_str}{Color.green} ]{Color.reset}")
+                print(f"{Color.green}\t    -> {Color.reset}{Color.bold}{url} {Color.reset}| {Color.bold}{Color.green}[ {technologies_str}{Color.green} ]{Color.reset}")
 
-                # Tulis hasil ke file
+                # Write to file
                 output_file.write(f"{url} | {technologies_str}\n")
 
     except FileNotFoundError:
