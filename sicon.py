8ad66a7a23478fd96f4d307fb2339330c8f17100
Valeforr <hirukaqghost@gmail.com>
2025-01-19 09:23:46 +0700

    Update sicon.py
    Adding multiple scan

--- sicon.py
+++ sicon.py
@@ -1,9 +1,8 @@
 # importing dependencies
-import sys
+import sys, argparse, sys
 from core.color import Color
 from core.check import check
-from core.utility import logo
-from core.utility import break_and_help
+from core.utility import logo, break_and_help
 from scan.wafscan import waf_scanning
 from scan.portscan import port_scanning
 from scan.subscan import subdo_scanning
@@ -13,33 +12,48 @@ from scan.techscan import more_info
 from scan.wp.wp_enum import wp_enum
 
 def main():
-    # 2.1 checking another tools that help this code run
+    logo()
+    parser = argparse.ArgumentParser(description=break_and_help(), usage="""\nsicon -u site.com -o waf subdo     only do waf scanning and subdomain scanning\nsicon -u site.com -o scan_dir      only do waf scanning and subdomain scanning\nsicon -u site.com -o port subdo    only do port scanning and subdomain scanning\nsicon -u site.com                  scan with all options """)
+    parser.add_argument("-o", "--option", nargs="+", choices=["waf", "port", "subdo", "scan_dir"], default=["all"], help="Choose one scan option, eg: ")
+    parser.add_argument("-u", "--url", required=True, help="url target")
+    parser.add_argument("-a", "--user-agent", help="custom user-agent in scan dir, cms detection, technology detection")
+    
+    if len(sys.argv) == 1:
+        parser.print_help()
+        sys.exit(1)
+
+    command = parser.parse_args()
+    url_target = command.url
+    options = command.option
+    ag = command.user_agent
+    
     list_tool = ['nmap','wafw00f','sublist3r','subfinder','assetfinder','dirsearch','httprobe']
     for tool in list_tool:
         check(tool)
-    logo()
-    # 2.2 argsparser 
-    command_arguments = sys.argv[1:]
-    if (len(command_arguments) > 0):
-        flag = command_arguments[0].upper()
-        if flag == "-U" or flag == "--URL":
-            URL_TARGET = command_arguments[1]
-            # 2.3 scanning
-            try:
-                print(f"{Color.bold}{Color.green}\n\t[*] Starting recon on : {URL_TARGET}{Color.reset}")
-                waf_scanning(URL_TARGET)
-                port_scanning(URL_TARGET)
-                subdo_scanning(URL_TARGET)
-                more_info(URL_TARGET)
-                cms_detection(URL_TARGET)
-                wp_enum(URL_TARGET)
-                scan_dir(URL_TARGET)
-            except KeyboardInterrupt:
-                print(f"{Color.bold}[!] Keyboard Interupt\n[!] Exit ... {Color.reset}")
-        else:
-            break_and_help()
-    else:
-        break_and_help()
+        
 
+    if "all" in options:
+        print(f"{Color.bold}{Color.green}\n\t[*] Starting recon on : {url_target}{Color.reset}")
+        waf_scanning(url_target)
+        port_scanning(url_target)
+        subdo_scanning(url_target)
+        more_info(url_target, ag)
+        cms_detection(url_target, ag)
+        wp_enum(url_target, ag)
+        scan_dir(url_target, ag)
+    else:
+        print(f"{Color.bold}{Color.green}\n\t[*] Starting recon on : {url_target}{Color.reset}")
+        if "waf" in options:
+            waf_scanning(url_target)
+        if "port" in options:
+            port_scanning(url_target)
+        if "subdo" in options:
+            subdo_scanning(url_target)
+            more_info(url_target, ag)
+            cms_detection(url_target, ag)
+            wp_enum(url_target, ag)
+        if "scan_dir" in options:
+            scan_dir(url_target ,ag)
+    
 if __name__ == "__main__":
     main()
