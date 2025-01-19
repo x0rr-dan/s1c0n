094758872a23c794995c95b760a1d91ec2b781cd
0x86 <51450260+x0rr-dan@users.noreply.github.com>
2025-01-19 14:07:27 +0700

    Update wp_enum.py
    add custom proxy and custom user agent

--- scan/wp/wp_enum.py
+++ scan/wp/wp_enum.py
@@ -1,9 +1,15 @@
 from core.color import Color
 from scan.wp.wppluggin import wpplugin
+from core.random_ag import rangent
 from os import path
 import time
 
-def wp_enum(target):
+def wp_enum(target, user_agent=None, proxy=None):
+    he = rangent()
+    if user_agent:
+        pass
+    else:
+        user_agent = he
     # read wordpress file that save in folder report
     if path.exists(f"report_{target}/wp.txt"):
         with open(f"report_{target}/wp.txt", 'r', encoding='utf-8') as r:
@@ -11,7 +17,7 @@ def wp_enum(target):
             print(f"{Color.bold}{Color.green}\n\t  [+] WORDPRESS ENUMERATION: {len(read_subdo)} sites{Color.reset}")
             for i in read_subdo:
                 i = i.strip()
-                wpplugin(i)
+                wpplugin(i, user_agent, proxy)
                 time.sleep(5)
     else:
         pass
